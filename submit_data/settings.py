aa427450baf8d49c27e7027a88b3df57f1f71dbb
Patricio Sard <patricio.sard@alumnos.usm.cl>
2017-11-04 03:02:14 -0300

    Se agrego log in de users

--- submit_data/settings.py
+++ submit_data/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'submit_data.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates',],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+
+LOGIN_REDIRECT_URL = '/'
